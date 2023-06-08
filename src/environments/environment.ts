000e8daefc144700fff44fda171fee17c000ada2
cemk31 <kanmaz.cem@googlemail.com>
2023-06-08 16:11:37 +0200

    production setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  backend: 'http://localhost:3000',
+  url: {
+    login: '/auth/signin',
+    register: '/auth/signup',
+  }
 };
 
 /*
