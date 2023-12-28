6b812b16ba95a946597df38ff634b16c62c4b416
cemk31 <kanmaz.cem@googlemail.com>
2023-12-28 12:31:10 +0100

    WP-329-Update-order-table

--- src/app/components/order/order-table/order-table.component.ts
+++ src/app/components/order/order-table/order-table.component.ts
@@ -5,6 +5,11 @@ import { throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { environment } from '../../../../environments/environment';
 import { format } from 'date-fns';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
+
 
 @Component({
   selector: 'app-order-table',
@@ -35,8 +40,8 @@ export class OrderTableComponent implements OnInit {
 
   showDetails: boolean[] = [];
 
-  toggleDetails(index: number): void {
-    this.showDetails[index] = !this.showDetails[index];
+  closeDetails(id: number) {
+    this.selectedId = null;
   }
 
   openDetails(id: number) {
@@ -59,14 +64,19 @@ export class OrderTableComponent implements OnInit {
           order.actualStatus = order.actualStatus.toLowerCase();
           if (order.actualStatus === 'received') {
             order.actualStatus = 'Empfangen';
+            order.actualStatusColor = 'success';
           } else if (order.actualStatus === 'planned') {
             order.actualStatus = 'In planung';
+            order.actualStatusColor = 'warning';
           } else if (order.actualStatus === 'postponed') {
             order.actualStatus = 'Verschoben';
+            order.actualStatusColor = 'warning';
           } else if (order.actualStatus === 'closed') {
             order.actualStatus = 'Abgeschlossen';
+            order.actualStatusColor = 'success';
           } else if (order.actualStatus === 'cancelled') {
             order.actualStatus = 'Storno';
+            order.actualStatusColor = 'danger';
           }
         });
 
@@ -93,4 +103,158 @@ export class OrderTableComponent implements OnInit {
     }
     return format(date, 'dd.MM.yyyy');
   }
+
+  ascending : boolean;
+  sortById() {
+    if (!this.ascending) {
+      this.orders.sort((a, b) => {
+        this.ascending = true;
+        return a.id - b.id;
+      });
+    } else {
+      this.orders.sort((a, b) => {
+        this.ascending = false;
+        return b.id - a.id;
+      });
+    }
+  }
+
+
+
+  showOnlyOpenOrdersChanged() {
+    this.filteredOrders = this.orders.filter(order => order.actualStatus !== 'Abgeschlossen' && order.actualStatus !== 'Storno');
+  }
+
+  selectedOrders: Set<number> = new Set();
+  disableDocumentExport = true;
+  selectAllOrders() {
+    if (this.selectedOrders.size !== this.orders.length) {
+      this.selectedOrders.clear();
+      this.orders.forEach(order => this.selectedOrders.add(order.id));
+      this.disableDocumentExport = false;
+    } else {
+      this.selectedOrders.clear();
+      this.disableDocumentExport = true;
+    }
+  }
+  
+  checkBoxOrder(id: number) {
+    if (this.selectedOrders.has(id)) {
+      this.selectedOrders.delete(id);
+      if (this.selectedOrders.size === 0) {
+        this.disableDocumentExport = true;
+      }
+    } else {
+      this.selectedOrders.add(id);
+      this.disableDocumentExport = false;
+    }
+  }
+  
+  setCheckboxState(id: number): boolean {
+    return this.selectedOrders.has(id);
+  }
+
+  //TODO: Create order Button
+  createOrder() {
+    
+  }
+
+  //TODO: Implement quick edit
+  quickEdit() {
+    if (this.selectedOrders.size === 1) {
+      this.selectedOrders.forEach(id => {
+        this.selectedId = id;
+      });
+    } else {
+      this.selectedId = null;
+    }
+  }
+  //TODO: Implement delete
+  delete() {
+    this.selectedOrders.forEach(id => {
+      
+      this.http.delete(environment.backend + environment.url.ista.order + '/' + id).subscribe();
+    });
+    this.getIstaOrders();
+  }
+
+  exportSelectedToExcel() {
+    // Filter the userRecords array based on the selected orders
+    const selectedOrders = this.selectedOrders.size > 0 ? this.orders.filter(order => this.selectedOrders.has(order.id)) : this.orders;
+
+    // Create a new workbook
+    const workbook = XLSX.utils.book_new();
+
+    // Convert the selected orders to a worksheet
+    const worksheet = XLSX.utils.json_to_sheet(selectedOrders);
+
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Selected Orders');
+
+    // Generate the Excel file
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    // Convert the Excel buffer to a Blob
+    const excelBlob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+    // Save the Excel file as a download
+    saveAs(excelBlob, 'selected_orders.xlsx');
+  }
+
+  //TODO: Export to PDF
+  exportSelectedToPdf() {
+    if (!this.orders || !this.selectedOrders) return;
+  
+    const selectedOrders = this.selectedOrders.size > 0 ? this.orders.filter(order => this.selectedOrders.has(order.id)) : this.orders;
+  
+    const doc = new jsPDF();
+  
+    const headers = [['ID', 'Status', 'Property Number', 'Company', 'Customer Contact', 'City', 'Phone', 'Mobile', 'Email']];
+  
+    const data = selectedOrders.map(order => [order.id, order.actualStatus, order.propertyNumber, order.company, order.customerContact, order.city, order.phone, order.mobile, order.email]);
+  
+    // const tableStyle = {
+    //   margin: { top: 10 },
+    //   headStyles: { fillColor: [41, 128, 185], textColor: 255, fontSize: 12 },
+    //   bodyStyles: { fontSize: 10 },
+    //   columnStyles: { 0: { cellWidth: 'auto' }, 1: { cellWidth: 'auto' }, /* weitere Spaltenkonfigurationen */ }
+    // };
+  
+    autoTable(doc, {
+      head: headers,
+      body: data,
+      margin: { top: 10 },
+      headStyles: { fillColor: [41, 128, 185], textColor: 255, fontSize: 12 },
+      bodyStyles: { fontSize: 10 },
+      columnStyles: { 0: { cellWidth: 'auto' }, 1: { cellWidth: 'auto' }, /* weitere Spaltenkonfigurationen */ }
+      // ...tableStyle
+    });
+    doc.save('selected_orders.pdf');  
+  }
+  
+  getSelectedOrders() {
+    return this.selectedOrders.size > 0 
+      ? this.orders.filter(order => this.selectedOrders.has(order.id))
+      : this.orders;
+  }
+
+  exportToPDF(id: Number) {
+    this.orders.forEach(order => {
+      if (order.id === id) {
+        const doc = new jsPDF();
+        const headers = [['ID', 'Status', 'Property Number', 'Company', 'Customer Contact', 'City', 'Phone', 'Mobile', 'Email']];
+        const data = [[order.id, order.actualStatus, order.propertyNumber, order.company, order.customerContact, order.city, order.phone, order.mobile, order.email]];
+        autoTable(doc, {
+          head: headers,
+          body: data,
+          margin: { top: 10 },
+          headStyles: { fillColor: [41, 128, 185], textColor: 255, fontSize: 12 },
+          bodyStyles: { fontSize: 10 },
+          columnStyles: { 0: { cellWidth: 'auto' }, 1: { cellWidth: 'auto' }, /* weitere Spaltenkonfigurationen */ }
+        });
+        doc.save('order_' + order.id + '.pdf');
+      }
+    });
+  }
 }
+  
